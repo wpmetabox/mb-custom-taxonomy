8f7144c2a742b23d7aca58e26998d9af167fa0cd
Anh <anh@fitwp.com>
2017-03-08 11:05:20 +0700

    Update deployment script

--- scripts/wporg-deploy.sh
+++ scripts/wporg-deploy.sh
@@ -1,8 +1,8 @@
-svn co -q "https://plugins.svn.wordpress.org/mb-custom-taxonomy/trunk" svn
+svn co "https://plugins.svn.wordpress.org/mb-custom-taxonomy/trunk" svn
 cp svn/.svn . -r
 rm -rf svn
 svn cleanup
 svn stat | grep '^?' | awk '{print $2}' | xargs -d "\n" svn add
 svn stat | grep '^!' | awk '{print $2}' | xargs -d "\n" svn rm --force
 svn ci --no-auth-cache --username rilwis --password $WP_ORG_PASSWORD -m "Version $npm_package_version"
-svn copy https://plugins.svn.wordpress.org/mb-custom-taxonomy/trunk https://plugins.svn.wordpress.org/mb-custom-taxonomy/tags/$npm_package_version -m "Version $npm_package_version"
+svn cp https://plugins.svn.wordpress.org/mb-custom-taxonomy/trunk https://plugins.svn.wordpress.org/mb-custom-taxonomy/tags/$npm_package_version --no-auth-cache --username rilwis --password $WP_ORG_PASSWORD -m "Version $npm_package_version"
