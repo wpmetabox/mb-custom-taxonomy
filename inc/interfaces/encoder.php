37faeef3558d05b88fd2009dc02c6b76a28fa3a9
Truong Giang <truongwp@gmail.com>
2017-08-21 10:21:04 +0700

    Fix coding standard

--- inc/interfaces/encoder.php
+++ inc/interfaces/encoder.php
@@ -9,6 +9,12 @@
 if ( ! interface_exists( 'MB_CPT_Encoder_Interface' ) ) {
 	interface MB_CPT_Encoder_Interface {
 
+		/**
+		 * Encode data.
+		 *
+		 * @param  array $data Data to encode.
+		 * @return string      Encoded string.
+		 */
 		public function encode( $data );
 	}
 }
